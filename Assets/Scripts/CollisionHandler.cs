cb20734d05dea621948884c905a071d490841cf1
Javier Rojas <javierito.rojas.ramirez@gmail.com>
2025-07-08 18:24:57 +0100

    Stop Movement.cs (Player Controls Input) once losing or winning.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,6 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private float levelLoadDelay = 3f;
+    
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -11,20 +13,24 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Everything is looking good!");
                 break;
             case "Finish":
-                LoadNextLevel();
+                StartSuccessSequence();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
     }
 
-    void LoadNextLevel()
+    private void StartCrashSequence()
     {
-        int currentScene = SceneManager.GetActiveScene().buildIndex;
-        int nextScene = (currentScene + 1) % SceneManager.sceneCountInBuildSettings;
-
-        SceneManager.LoadScene(nextScene);
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+    
+    private void StartSuccessSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
     }
 
     void ReloadLevel()
@@ -32,4 +38,13 @@ public class CollisionHandler : MonoBehaviour
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentScene);
     }
+    
+    void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = (currentScene + 1) % SceneManager.sceneCountInBuildSettings;
+
+        SceneManager.LoadScene(nextScene);
+    }
+    
 }
