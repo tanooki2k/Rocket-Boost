517413933b4f4dbb9269da0764706855bb95b35a
Javier Rojas <javierito.rojas.ramirez@gmail.com>
2025-07-09 12:22:28 +0100

    Multiple SFX added: Crash and Win.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] private float levelLoadDelay = 3f;
-    
+    [SerializeField] private AudioClip success;
+    [SerializeField] private AudioClip crash;
+
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -23,12 +33,14 @@ public class CollisionHandler : MonoBehaviour
 
     private void StartCrashSequence()
     {
+        _audioSource.PlayOneShot(crash);
         GetComponent<Movement>().enabled = false;
         Invoke("ReloadLevel", levelLoadDelay);
     }
     
     private void StartSuccessSequence()
     {
+        _audioSource.PlayOneShot(success);
         GetComponent<Movement>().enabled = false;
         Invoke("LoadNextLevel", levelLoadDelay);
     }
